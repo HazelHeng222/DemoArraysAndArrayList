9ba4f4e91a6c8e9d884704ae9bb14dc08d39f5a3
20029318 <20029318@myrp.edu.sg>
2021-06-03 13:06:32 +0800

    learned android studio update: fruits.set(fruits.size()-1, "dragon fruit"); Java update: fruits.set(-1, "dragon fruit");

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -17,8 +17,32 @@ public class ArrayActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView1);
 
         //continue view the code in the worksheet
+        String [] fruits = new String [3];
+
+        fruits[0] = "apple";
+        fruits[1] = "banana";
+        fruits[2] = "cherry";
+
+        //del
+        fruits[2] = null;
+
+        //update
+        fruits[1] = "blueberry";
+
+        //add
+        fruits[2] = "grape";
 
 
+
+        System.out.println("Size is : " + fruits.length);
+
+        String msg = "";
+        for (int i = 0; i < fruits.length; i ++){
+            msg += fruits[i] + "\n";
+        }
+
+        System.out.println(msg);
+        tv.setText("Fruits\n=====\n" + msg);
 
     }
 }
--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
@@ -17,6 +19,34 @@ public class ArrayListActivity extends AppCompatActivity {
         tv = findViewById(R.id.textView2);
 
         //continue view the code in the worksheet
+        ArrayList<String> fruits = new ArrayList<String>();
+
+        fruits.add("apple");
+        fruits.add("banana");
+        fruits.add("cherry");
+
+        //add - ArrayList.add(String/object)
+        fruits.add("durian");
+
+        //update - ArrayList.set(index, datatype)
+        fruits.set(fruits.size()-1, "dragon fruit");
+
+        //delete - ArrayList.remove(index)
+        fruits.remove(0);
+
+        //get and assign
+        String theFruit = fruits.get(1);
+
+
+        System.out.println("Size is : " + fruits.size());
+
+        String msg = "";
+        for (int i = 0; i < fruits.size(); i ++){
+            msg += fruits.get(i) + "\n";
+        }
+
+        System.out.println(msg);
+        tv.setText("Fruits\n=====\n" + msg);
 
 
     }
